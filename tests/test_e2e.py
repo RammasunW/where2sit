0da4329616b537e28ef93bb2b9c7c2612ffce1f6
CameronOnGit <cameron.jardine31@gmail.com>
2026-04-19 11:21:32 -0400

    Resolving adds

---
+++ tests/test_e2e.py
@@ -0,0 +1,11 @@
+#import pytest
+
+# below to resolve error async error/ toggle comment
+#import os
+#os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
+# @pytest.mark.django_db
+# def test_homepage_loads(page, live_server):
+#     page.goto(live_server.url)
+
+#     assert page.locator("h1").inner_text() == "Room List"
